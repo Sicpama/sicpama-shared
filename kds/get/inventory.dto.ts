b5c5446334610d0074764ccd824bcea0e3f4b662
Jung Dae Suh <jungdaesuh1221@gmail.com>
2024-03-28 09:21:23 +0900

    chore: updated inventory dtos

--- kds/get/inventory.dto.ts
+++ kds/get/inventory.dto.ts
@@ -1,8 +1,29 @@
-export interface KdsInventoryMenuAndMenuQuantities {
+import { KdsPaginationResponse } from './base.dto';
+
+export interface KdsInventoryDomainDto extends KdsPaginationResponse {
+  data: KdsInventoryDomain[];
+}
+
+export class InventoryDomain implements KdsInventoryDomain {
   id: number;
-  menuId: number;
-  menuName: string;
-  requiredQuantity: number;
+  name: string;
+  quantity: number;
+  storeId: number;
+  menuAndMenuQuantities: KdsInventoryMenuAndMenuQuantities[];
+
+  constructor(params: {
+    id: number;
+    name: string;
+    quantity: number;
+    storeId: number;
+    menuAndMenuQuantities: KdsInventoryMenuAndMenuQuantities[];
+  }) {
+    this.id = params.id;
+    this.name = params.name;
+    this.quantity = params.quantity;
+    this.storeId = params.storeId;
+    this.menuAndMenuQuantities = params.menuAndMenuQuantities;
+  }
 }
 
 export interface KdsInventoryDomain {
@@ -11,4 +32,11 @@ export interface KdsInventoryDomain {
   quantity: number;
   storeId: number;
   menuAndMenuQuantities: KdsInventoryMenuAndMenuQuantities[];
+}
+
+export interface KdsInventoryMenuAndMenuQuantities {
+  id: number;
+  menuId: number;
+  menuName: string;
+  menuQuantityRequired: number;
 }
