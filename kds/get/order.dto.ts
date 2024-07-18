4f0e9c98a014f758beb3816470ba8483ff653c5e
Chris Hewison <hewisonchris@gmail.com>
2024-07-18 15:01:23 +0900

    chore: remove some legacy code

--- kds/get/order.dto.ts
+++ kds/get/order.dto.ts
@@ -1,10 +1,51 @@
+import { KdsAdditionalLanguageDto, MenuSellLabel } from '../..';
 import { PaymentGatewayType } from '../../enums/payment.enum';
 import { OrderItemStatus } from '../../enums/status.enum';
 
 import { KdsPaginationResponse } from './base.dto';
-import { KdsBaseMenuSnapshotResDto } from './menu.legacy.dto';
 import { KdsStoreTableOrderResult } from './store-order.dto';
 
+export interface KdsBaseMenuResDto {
+  id: number;
+  name: string;
+  description: string | null;
+  price: number;
+  discountPrice: number | null;
+  discountDays: string | null;
+  isTodayDiscounted: boolean;
+  photo: string | null;
+  isShareable: boolean;
+  isAvailable: boolean;
+  sellLabel: MenuSellLabel | null;
+  priority: number | null;
+  storeId: number;
+  categoryId: number;
+  currency: string;
+  minOrderQuantity: number;
+  additionalLanguages?: KdsAdditionalLanguageDto[];
+}
+
+export interface KdsBaseMenuOptionChoiceResDto {
+  id: number;
+  name: string;
+  price: number;
+  description: string | null;
+  additionalLanguages?: KdsAdditionalLanguageDto[];
+}
+
+export interface KdsBaseMenuOptionResDto {
+  id: number;
+  name: string;
+  description: string | null;
+  minChoices: number;
+  maxChoices: number;
+  choices: KdsBaseMenuOptionChoiceResDto[];
+  additionalLanguages?: KdsAdditionalLanguageDto[];
+}
+
+export interface KdsBaseMenuSnapshotResDto extends KdsBaseMenuResDto {
+  menuOptions: KdsBaseMenuOptionResDto[];
+}
 export interface KdsPayment {
   customerId: string;
   orderId: string;
